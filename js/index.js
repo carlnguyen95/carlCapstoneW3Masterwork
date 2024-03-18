68ee2f317c75f5da249c25a35576e0a8a7651ce0
carlnguyen95 <binguyen5919@gmail.com>
2024-03-18 12:05:11 +0700

    Finish dark mode for home page

--- js/index.js
+++ js/index.js
@@ -1,9 +1,25 @@
-function toggleTheme() {
-  var darkThemeElements = document.getElementsByClassName("dark-theme-toggle");
+var root = document.querySelector(":root");
 
-  for (var element of darkThemeElements) {
-    element.classList.toggle("bg-dark");
-  }
+function toggleThemeDark() {
+  var toggleThemeElement = document.getElementById("theme-toggle");
+  root.style.setProperty("--light-bg-color", "#191C1D");
+  root.style.setProperty("--bg-color", "#131617");
+  root.style.setProperty("--title-color", "#EEE");
+  root.style.setProperty("--text-color", "#B7B7B7");
+  toggleThemeElement.innerText = "clear_day";
+  toggleThemeElement.onclick = toggleThemeLight;
+  toggleThemeElement.classList.toggle("text-white");
+  console.log("Changed to dark mode");
+}
 
-  document.getElementById("theme-toggle").classList.toggle("text-white");
+function toggleThemeLight() {
+  var toggleThemeElement = document.getElementById("theme-toggle");
+  root.style.setProperty("--light-bg-color", "#F7F9FC");
+  root.style.setProperty("--bg-color", "#FFF");
+  root.style.setProperty("--title-color", "#2C3038");
+  root.style.setProperty("--text-color", "#5A5A5A");
+  toggleThemeElement.innerText = "dark_mode";
+  toggleThemeElement.onclick = toggleThemeDark;
+  toggleThemeElement.classList.toggle("text-white");
+  console.log("Changed to light mode");
 }
